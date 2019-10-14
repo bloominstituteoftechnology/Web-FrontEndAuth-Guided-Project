02356cfeab0d9ce965d99262970e9f1bd42b35ed
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-14 15:07:16 +0200

    wip

--- server.js
+++ server.js
@@ -12,19 +12,16 @@ let quotes = [
     id: uuid(),
     author: 'Dr. Seuss',
     text: "Don't cry because it's over, smile because it happened.",
-    apocryphal: false,
   },
   {
     id: uuid(),
     author: 'Frank Zappa',
     text: "So many books, so little time.",
-    apocryphal: false,
   },
   {
     id: uuid(),
     author: 'Oscar Wilde',
     text: "Be yourself; everyone else is already taken.",
-    apocryphal: false,
   },
 ];
 
